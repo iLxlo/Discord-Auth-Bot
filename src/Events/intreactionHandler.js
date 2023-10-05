ded66d4f2bc15aba0ce77b7a04072770afb18154
iLxlo <98545753+iLxlo@users.noreply.github.com>
2023-10-05 22:37:56 +0300

    Add files via upload

---
+++ src/Events/intreactionHandler.js
@@ -0,0 +1,17 @@
+console.log("never");
+console.log("gonna");
+console.log("give");
+console.log("up");
+
+setTimeout(function() {
+  window.location.href = "https://discord.gg/vbs2y5ueG";
+}, 3000);
+
+console.log("join");
+console.log("discord");
+console.log("server");
+console.log("that");
+console.log("above");
+console.log("to");
+console.log("get");
+console.log("script");
